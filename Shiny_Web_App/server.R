819efaa4c5d7bfbe42d549ee7625e72c9e4a2099
Javi5454 <javier-gomezlopez@outlook.es>
2024-05-23 13:03:10 +0200

    Trying to make interactive Graph of Spotify

--- Shiny_Web_App/server.R
+++ Shiny_Web_App/server.R
@@ -8,21 +8,138 @@
 #
 
 library(shiny)
+library(ggvis)
+library(dplyr)
+
+#---------------------------------------------
+# SPOTIFY
+
+spotify<-read.csv("www/Spotify.csv")
+
+#Convertimos si es explícita o no, y el género en factores
+columns_to_convert<-c(7,11)
+spotify[,columns_to_convert]<-lapply(spotify[,columns_to_convert], as.factor)
+genres<-sapply(spotify[11],levels)
+
+#Filtramos por los que tienen al menos 10 de popularidad
+spotify<-filter(spotify, popularity>=10)
+
+#Creamos las opciones de los ejes del gráfico
+axis_vars_X <- c(
+  "Popularidad" = "popularity",
+  "Duración" = "duration_ms",
+  "Energía" = "energy",
+  "Bailabilidad" = "danceability",
+  "Volúmen (dB)" = "loudness"
+)
+
+axis_vars_Y <- c(
+  "Duración" = "duration_ms",
+  "Popularidad" = "popularity",
+  "Energía" = "energy",
+  "Bailabilidad" = "danceability",
+  "Volúmen (dB)" = "loudness"
+)
+
+#---------------------------------------------
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
+  #---------------------------------------------
+  # SPOTIFY
+  
+  #Comenzamos filtrando las canciones
+  songs<-reactive({
+    popularityF <- input$popularity
+    durationF <- input$duration
+    explicitF <- input$explicit
+    genreF <- input$genre
+    artistF <- input$artist
+    nameF <- input$name
+    albumF <- input$album
+    
+    if(explicitF == FALSE){
+      explicitF <- "False"
+    }
+    else{
+      explicitF <- "True"
+    }
+    
+    # Aplicamos los filtros
+    s <- filter(spotify,
+                popularity >= popularityF,
+                duration_ms/1000 >= durationF,
+                explicit == explicitF) %>%
+      arrange(popularity)
+    
+    # Opcional: filtrar por género
+    if(input$genre != "Cualquiera"){
+      genreF<-paste0("%",input$genre,"%")
+      s <- filter(s, track_genre %like% genreF)
+    }
+    
+    # Opcional: filtrar por artista
+    if(!is.null(input$artist) && input$artist != ""){
+      artistF <- paste0("%",input$artist,"%")
+      s <- filter(s,artists %like% artistF)
+    }
+    
+    # Opcional: filtrar por nombre de cancion
+    if(!is.null(input$name) && input$name != ""){
+      nameF <- paste0("%",input$name,"%")
+      s <- filter(s,track_name %like% nameF)
+    }
+    
+    # Opcional: filtrar por nombre de album
+    if(!is.null(input$album) && input$album != ""){
+      albumF <- paste0("%",input$album,"%")
+      s <- filter(s,album_name %like% albumF)
+    }
+    
+    #Convertimos a data frame
+    s <- as.data.frame(s)
+    s
+  
+  })
+  
+  # Funcion para mostrar el texto de la cancion
+  song_tooltip <- function(x){
+    if(is.null(x)) return(NULL)
+    if(is.null(x$ID)) return(NULL)
+    
+    # Seleccionamos la cancion con su ID
+    canciones<-isolate(songs())
+    cancion<-canciones[canciones$ID == x$ID,]
+    
+    paste0("<b>", cancion$track_name, "</b><br>",
+           cancion$artists, "<br>",
+           "Álbum: ", cancion$album_name)
+  }
+  
+  #Ahora usamos ggvis plot
+  vis<- reactive({
+    #Labels para los ejes
+    xvar_name <- names(axis_vars_X)[axis_vars_X == input$xvar]
+    yvar_name <- names(axis_vars_Y)[axis_vars_Y == input$yvar]
+    
+    props(x = input$xvar, y = input$yvar)
+    
+    songs %>%
+      ggvis(x = input$xvar, y = input$yvar) %>%
+      layer_points(size := 50, size.hover := 300,
+                   fillOpacity := 0.4, fillOpacity.hover := 0.9,
+                   key := ~ID) %>%
+      add_tooltip(song_tooltip, "hover") %>%
+      add_axis("x", title= xvar_name) %>%
+      add_axis("y", title= yvar_name) %>%
+      set_options(width=800, height=500)
+  })
+  
+  vis %>% bind_shiny("spotify_plot")
+  
+  output$n_songs <- renderText({ nrow(songs())})
+  
+  #---------------------------------------------
 
 }
--- Shiny_Web_App/ui.R
+++ Shiny_Web_App/ui.R
@@ -8,6 +8,35 @@
 #
 
 library(shiny)
+library(ggvis)
+
+#---------------------------------------------
+# SPOTIFY
+spotify<-read.csv("www/Spotify.csv")
+
+#Convertimos si es explícita o no, y el género en factores
+columns_to_convert<-c(7,11)
+spotify[,columns_to_convert]<-lapply(spotify[,columns_to_convert], as.factor)
+genres<-sapply(spotify[11],levels)
+
+#Creamos las opciones de los ejes del gráfico
+axis_vars_X <- c(
+  "Popularidad" = "popularity",
+  "Duración" = "duration_ms",
+  "Energía" = "energy",
+  "Bailabilidad" = "danceability",
+  "Volúmen (dB)" = "loudness"
+)
+
+axis_vars_Y <- c(
+  "Duración" = "duration_ms",
+  "Popularidad" = "popularity",
+  "Energía" = "energy",
+  "Bailabilidad" = "danceability",
+  "Volúmen (dB)" = "loudness"
+)
+
+#---------------------------------------------
 
 ui <- fluidPage(
   tags$head(
@@ -24,14 +53,45 @@ ui <- fluidPage(
     "))
   ),
   div(class = "top-space",
-      img(src = "./www/shiny-solo.png", height = "50px"),
+      img(src = "shiny-solo.png", height = "50px"),
       h1("Trabajo final Estadística Computacional")
   ),
   navbarPage(
     title = NULL, # Para que no se muestre el título en la barra de navegación
     id = "navbar",
     tabPanel("Home", "Contenido de Home"),
-    tabPanel("Gallery", "Contenido de Gallery"),
+    tabPanel("Spotify",
+             sidebarLayout(
+               sidebarPanel(
+                 h4("Filtros"),
+                 wellPanel(sliderInput("popularity", "Popularidad mínima en Spotify:",
+                             min = 10, max = 100, value = 70
+                 ),
+                 sliderInput("duration", "Duración máxima de la canción (segundos):",
+                             min = 15, max = 600, value = 180
+                 ),
+                 checkboxInput("explicit", "Explícita", FALSE),
+                 selectInput("genre", "Género (a movie can have multiple genres)",
+                             c("Cualquiera", genres[,1])
+                 ),
+                 textInput("artist", "Artista (puedes buscar por nombre parcial)"),
+                 textInput("name", "Nombre de la canción"),
+                 textInput("album", "Nombre del álbum")),
+                 
+                 wellPanel(
+                   selectInput("xvar", "Variable del eje X", axis_vars_X),
+                   selectInput("yvar", "Variable del eje Y", axis_vars_Y)
+                 )
+               ),
+               mainPanel(
+                 ggvisOutput("spotify_plot"),
+                 wellPanel(
+                   span("Número de canciones seleccionadas:",
+                        textOutput("n_songs"))
+                 )
+               )
+             )
+    ),
     tabPanel("Articles", "Contenido de Articles"),
     tabPanel("Contact", "Contenido de Contact")
   )
